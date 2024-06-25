332678efb4b93216b1cff20845b46635bce5913f
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-06-25 13:06:27 +0530

    add keyboard functionality & clean code
    - added keyboard functionality using keyup event listener
    - moved code of 'clear', 'back', '.', '=' buttons into functions

--- extra.js
+++ extra.js
@@ -28,3 +28,23 @@ switch (inputArray.length){
     default:
       console.log('You have messed up');
   }
+
+
+switch (true) {
+  case '.': ;
+  case 0: onClickingDigit(0);
+  case 1: onClickingDigit(1);
+  case 2: onClickingDigit(2);
+  case 3: onClickingDigit(3);
+  case 4: onClickingDigit(4);
+  case 5: onClickingDigit(5);
+  case 6: onClickingDigit(6);
+  case 7: onClickingDigit(7);
+  case 8: onClickingDigit(8);
+  case 9: onClickingDigit(9);
+  case '/': ;
+  case '*': onClickingOperator('*');
+  case '-': onClickingOperator('-');
+  case '+': onClickingOperator('+');
+  case "=": onClickingOperator('=');
+}
--- javascript.js
+++ javascript.js
@@ -25,14 +25,6 @@ function updateInputArray (){
   let decimalPlaceCounter = 0; // to know if '.' button has been clicked, and to store the decimal place count
   let isThisNum; // to know whether last clicked button is a digit or an operator
 
-  let dot = document.querySelector(`#dot`);
-  dot.addEventListener('click',() => {
-    if (!decimalPlaceCounter){
-      display.textContent = `${num}.`;
-      decimalPlaceCounter++;
-    }
-  });
-
   // to update number and display it on clicking the digit buttons
   function onClickingDigit (digit) {
     if (decimalPlaceCounter){
@@ -44,7 +36,7 @@ function updateInputArray (){
     display.textContent = `${num}`;
     isThisNum = true;
   }
-  
+
   // to update expression on clicking the operator buttons
   function onClickingOperator (operator) {
     if (!isThisNum){ //if last clicked button was also an operator, change it in input array with new operator
@@ -64,17 +56,22 @@ function updateInputArray (){
     }
     isThisNum = false;
   };
+  
+  function onClickingDot () {
+    if (!decimalPlaceCounter){
+      display.textContent = `${num}.`;
+      decimalPlaceCounter++;
+    }
+  }
 
-  let clear = document.querySelector('#clear');
-  clear.addEventListener('click', ()=> {
+  function onClickingClear() {
     inputArray = [];
     num = 0;
     decimalPlaceCounter = 0;
     display.textContent = num;
-  });
+  }
 
-  let back = document.querySelector('#back');
-  back.addEventListener('click', () => {
+  function onClickingBack (){
     if (!isThisNum) {
       num = inputArray[0];
       inputArray = [];
@@ -84,9 +81,44 @@ function updateInputArray (){
     num = num.toString().slice(0,-1);
     num = Number(num);
     display.textContent = num;
+  }
+
+  // Keyboard functionality
+  document.addEventListener('keyup', (event) => {
+    if (event.key == 'Delete') {onClickingClear()}
+    else if (event.key == 'Backspace') {onClickingBack()}
+    else if (event.key == '.') {onClickingDot()}
+    else if (event.key == '0') {onClickingDigit(0)}
+    else if (event.key == '1') {onClickingDigit(1)}
+    else if (event.key == '2') {onClickingDigit(2)}
+    else if (event.key == '3') {onClickingDigit(3)}
+    else if (event.key == '4') {onClickingDigit(4)}
+    else if (event.key == '5') {onClickingDigit(5)}
+    else if (event.key == '6') {onClickingDigit(6)}
+    else if (event.key == '7') {onClickingDigit(7)}
+    else if (event.key == '8') {onClickingDigit(8)}
+    else if (event.key == '9') {onClickingDigit(9)}
+    else if (event.key == '/') {onClickingOperator('/')}
+    else if (event.key == '*') {onClickingOperator('*')}
+    else if (event.key == '-') {onClickingOperator('-')}
+    else if (event.key == '+') {onClickingOperator('+')}
+    else if (event.key == '=') {onClickingOperator('=')}
+    else if (event.key == 'Enter') {onClickingOperator('=')}
   })
+
+  // On screen buttons functionality
+  let clear = document.querySelector('#clear');
+  clear.addEventListener('click', onClickingClear);
+
+  let back = document.querySelector('#back');
+  back.addEventListener('click', onClickingBack);
+
+  let dot = document.querySelector(`#dot`);
+  dot.addEventListener('click', onClickingDot);
+
+  let zero = document.querySelector('#zero');
+  zero.addEventListener('click', () => {onClickingDigit(0)})
   
-  // Adding event listeners for digit buttons
   let one = document.querySelector('#one');
   one.addEventListener('click', () => {onClickingDigit(1)})
 
@@ -114,21 +146,17 @@ function updateInputArray (){
   let nine = document.querySelector('#nine');
   nine.addEventListener('click', () => {onClickingDigit(9)})
 
-  let zero = document.querySelector('#zero');
-  zero.addEventListener('click', () => {onClickingDigit(0)})
-  
-  // Adding event listeners for operator buttons
-  let addition= document.querySelector('#addition');
-  addition.addEventListener('click', () => {onClickingOperator('+')})
-
-  let subtraction= document.querySelector('#subtraction');
-  subtraction.addEventListener('click', () => {onClickingOperator('-')})
+  let division= document.querySelector('#division');
+  division.addEventListener('click', () => {onClickingOperator('/')})
 
   let multiplication= document.querySelector('#multiplication');
   multiplication.addEventListener('click', () => {onClickingOperator('*')})
 
-  let division= document.querySelector('#division');
-  division.addEventListener('click', () => {onClickingOperator('/')})
+  let subtraction= document.querySelector('#subtraction');
+  subtraction.addEventListener('click', () => {onClickingOperator('-')})
+
+  let addition= document.querySelector('#addition');
+  addition.addEventListener('click', () => {onClickingOperator('+')})
 
   let evaluate= document.querySelector('#evaluate');
   evaluate.addEventListener('click', () => {onClickingOperator('=')})
